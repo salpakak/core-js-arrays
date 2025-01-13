550f8bf46c79fda61669ca772998ba5368f82fe0
EZAVOORRR <salpakak@gmail.com>
2025-01-13 17:37:53 +0300

    feat: solve distinct task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -255,8 +255,9 @@ function toStringList(arr) {
  *   distinct([ 1, 1, 2, 2, 3, 3, 4, 4]) => [ 1, 2, 3, 4]
  *   distinct([]) => []
  */
-function distinct(/* arr */) {
-  throw new Error('Not implemented');
+function distinct(arr) {
+  const newArr = new Set(arr);
+  return Array.from(newArr);
 }
 
 /**
