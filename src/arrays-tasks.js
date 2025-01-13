bd432ab68dda674484b2b77ccdf149fc7fe3c323
salpakak <salpakak@gmail.com>
2025-01-14 02:21:20 +0300

    feat: solve caclulateBalance task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -327,8 +327,10 @@ function selectMany(arr, childrenSelector) {
  *   calculateBalance([ [ 10, 8 ], [ 1, 5 ] ])  => (10 - 8) + (1 - 5) = 2 + -4 = -2
  *   calculateBalance([]) => 0
  */
-function calculateBalance(/* arr */) {
-  throw new Error('Not implemented');
+function calculateBalance(arr) {
+  return arr
+    .map(([income, expense]) => income - expense)
+    .reduce((total, balance) => total + balance, 0);
 }
 
 /**
