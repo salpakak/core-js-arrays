c0d3bc6316a1c0f9d1a97e2e6d75f8ba0db4c376
EZAVOORRR <salpakak@gmail.com>
2025-01-13 16:18:22 +0300

    feat: solve findAllOccurrences task

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -77,8 +77,12 @@ function findElement(arr, value) {
  *    findAllOccurrences([ null, undefined, null ], null) => 2
  *    findAllOccurrences([ true, 0, 1, 'true' ], true) => 1
  */
-function findAllOccurrences(/* arr, item */) {
-  throw new Error('Not implemented');
+function findAllOccurrences(arr, item) {
+  return arr.reduce(
+    (accumulator, element) =>
+      element === item ? accumulator + 1 : accumulator,
+    0
+  );
 }
 
 /**
